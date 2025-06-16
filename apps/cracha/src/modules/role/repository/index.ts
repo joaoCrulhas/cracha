c4e9601ff92c69f8bc7994d25513d147fa557fd9
joaocrulhas <joao.crulhas@gmail.com>
2025-06-16 08:23:22 +0200

    delete role

---
+++ apps/cracha/src/modules/role/repository/index.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import {
+  IRepositoryRead,
+  IRepositoryWrite,
+} from '../../../../types/repository/repository.type';
+import { DatabaseService } from '../../system/database/services/database.service';
+import { CreateRoleRequestDto, Role, UpdateRoleRequestDto } from '../dtos';
+
+@Injectable()
+export class RoleRepository
+  implements IRepositoryWrite<Role>, IRepositoryRead<Role>
+{
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async delete(id: number): Promise<Role> {
+    return this.databaseService.client.role.delete({
+      id,
+    });
+  }
+
+  update(id: number, input: UpdateRoleRequestDto): Promise<Role> {
+    return this.databaseService.client.role.update({
+      data: input,
+      where: {
+        id,
+      },
+    });
+  }
+
+  find(where: any): Promise<Role> {
+    throw new Error('Method not implemented.');
+  }
+  findById(id: number): Promise<Role> {
+    throw new Error('Method not implemented.');
+  }
+  async insert(input: CreateRoleRequestDto): Promise<Role> {
+    return this.databaseService.client.role.create({
+      data: input,
+    });
+  }
+}
