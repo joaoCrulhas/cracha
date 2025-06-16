66048ca0746cbcda178539da4611e0de5146c5bf
joaocrulhas <joao.crulhas@gmail.com>
2025-06-16 08:00:46 +0200

    delete role

---
+++ apps/cracha/src/modules/user/services/user-role.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { DatabaseService } from '../../system/database/services/database.service';
+
+type UserRoleArgs = { userId: number; roleId: number };
+
+@Injectable()
+export class UserRoleService {
+  constructor(private readonly databaseService: DatabaseService) {}
+  async relateUserRole({ userId, roleId }: UserRoleArgs) {
+    await this.databaseService.client.userRoles.create({
+      data: {
+        roleId,
+        userId,
+      },
+    });
+  }
+
+  async removeUserRole({ roleId, userId }: UserRoleArgs) {
+    await this.databaseService.client.userRoles.delete({
+      where: {
+        roleId_userId: {
+          roleId,
+          userId,
+        },
+      },
+    });
+  }
+
+  async getUserRoles(userId: number) {
+    return await this.databaseService.client.userRoles.findMany({
+      include: {
+        role: true,
+        user: true,
+      },
+      where: {
+        userId,
+      },
+    });
+  }
+}
---
+++ prisma/migrations/20250616054317_role_user/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "user_roles" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "roleId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "user_roles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_roles_roleId_userId_key" ON "user_roles"("roleId", "userId");
+
+-- AddForeignKey
+ALTER TABLE "user_roles" ADD CONSTRAINT "user_roles_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user_roles" ADD CONSTRAINT "user_roles_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "roles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
