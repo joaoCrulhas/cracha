b65401faae977c43707e26eef8dfa44097addf9f
joaocrulhas <joao.crulhas@gmail.com>
2025-06-19 23:17:18 +0200

    feat(action): create an on module init

--- apps/cracha/src/modules/action/services/action.service.ts
+++ apps/cracha/src/modules/action/services/action.service.ts
@@ -1,10 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ActionDto, CreateActionDto, UpdateActionDto } from '../dtos';
 import { DatabaseService } from '../../system/database/services/database.service';
+import { DEFAULT_ACTIONS } from '../types/constants';
 
 @Injectable()
-export class ActionService {
+export class ActionService implements OnModuleInit {
   constructor(private readonly databaseService: DatabaseService) {}
+
+  async onModuleInit() {
+    for (const actionName of DEFAULT_ACTIONS) {
+      console.log(actionName);
+      const hasAction = await this.databaseService.client.action.count({
+        where: {
+          name: actionName,
+        },
+      });
+      if (!hasAction) {
+        await this.databaseService.client.action.create({
+          data: {
+            name: actionName,
+          },
+        });
+      }
+    }
+  }
+
   async createAction(input: CreateActionDto): Promise<ActionDto> {
     const actionCreated = await this.databaseService.client.action.create({
       data: input,
@@ -13,11 +33,12 @@ export class ActionService {
   }
 
   async deleteAction(id: number): Promise<ActionDto> {
-    return await this.databaseService.client.action.delete({
+    const actionDeleted = await this.databaseService.client.action.delete({
       where: {
         id,
       },
     });
+    return ActionDto.fromPrisma(actionDeleted);
   }
 
   async updateAction(id: number, input: UpdateActionDto): Promise<ActionDto> {
