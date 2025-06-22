f4fce6499783981b6176e5a067217d9d92b2854c
joaocrulhas <joao.crulhas@gmail.com>
2025-06-22 15:54:09 +0200

    feat(user): add user to not have acess to dashboard

--- apps/cracha/src/modules/resource/controllers/resource.controller.ts
+++ apps/cracha/src/modules/resource/controllers/resource.controller.ts
@@ -6,6 +6,14 @@ import { ResourceService } from '../services';
 export class ResourceController {
   constructor(private readonly resourceService: ResourceService) {}
   @Post()
+  /* <<<<<<<<<<<<<<  ✨ Windsurf Command ⭐ >>>>>>>>>>>>>>>> */
+  /**
+   * Creates a new resource.
+   *
+   * @param input - The resource input data to create.
+   * @returns The created resource.
+   */
+  /* <<<<<<<<<<  fb37742b-ea9b-42db-be97-9916e03fe9bf  >>>>>>>>>>> */
   async create(@Body() input: CreateResourceRequestDto) {
     return await this.resourceService.create(input);
   }
